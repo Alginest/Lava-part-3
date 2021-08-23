e625e6a45a59e726e9de2519e31d9f0c1d43c52e
ado durak <76266634+Alginest@users.noreply.github.com>
2021-08-23 15:23:45 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+import Navbar from './components/Navbar'
+import SiteOptic from './components/SiteOptic'
+import MobInfo from './components/MobInfo'
+function App() {
+  return (
+    <>
+      <Navbar />
+      <SiteOptic />
+      <MobInfo />
+    </>
+  )
+}
+
+export default App
